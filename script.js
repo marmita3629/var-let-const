597c99bf834c89892c9dc8fdfbf17f374898d25a
Japa_cama <al.2009.andre@gmail.com>
2025-09-18 11:09:27 -0300

    add do codigo pedido

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+//var / let / const
+
+var nomeCantina = "Cantina da Escola";
+let salgados = 20;
+const precoSalgado = 5;
+
+salgados = salgados;let totalVendido = 5 * precoSalgado
+
+//consoles.log
+
+console.log ("Bem-vindo à" + nomeCantina);
+console.log ("temos" + salgados + "salgados disponíveis");
+console.log("Cada salgado custa R$" + precoSalgado)
+console.log("Agora restam " + salgados + " salgados.");
+console.log("A cantina vendeu R$" + totalVendido);
+
+
 
